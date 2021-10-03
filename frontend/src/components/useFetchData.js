a70f742d850945adfde09f7f0e2b51df4cbc6f85
Jasmin Katanic <34764465+jasminkatanic@users.noreply.github.com>
2021-10-03 15:26:27 +0200

    Wont merge untill approved

--- frontend/src/components/useFetchData.js
+++ frontend/src/components/useFetchData.js
@@ -2,15 +2,21 @@ import { useState } from "react/cjs/react.development";
 import axios from "axios";
 import { useEffect } from "react";
 
-const useFetchData = (url) => {
+const useFetchData = (url, category, pageOffset, searchTerm) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
-
-  useEffect(() => {
-    axios.get(url)       
-    .then(res => setData(res.data))
+  
+  useEffect(() => {        
+    axios.get(`${url}${category}?pageOffset=${pageOffset}${searchTerm ? `&search=${searchTerm}` : ''}`)       
+    .then(res =>  {
+      if(pageOffset>0){
+        setData([...data, ...res.data])
+      }else{
+        setData(res.data)
+      }
+    })      
     .catch(err => {setError(err)})
-  },[url])
+  },[url, category, pageOffset, searchTerm])
 
   return {data, error};
 }
